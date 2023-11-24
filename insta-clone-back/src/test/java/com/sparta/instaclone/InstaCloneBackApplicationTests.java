95a4accf7cbee34d442ca94f2a1a2a3643c4ff88
Hewllpark <141545917+Hewllpark@users.noreply.github.com>
2023-11-24 15:59:53 +0900

    Add files via upload
    뼈대

---
+++ insta-clone-back/src/test/java/com/sparta/instaclone/InstaCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.instaclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaCloneBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
